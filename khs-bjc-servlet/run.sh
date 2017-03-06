3216a31519deb7ff686d5928df94a67ec7a44b2b
Greg Emerick <gemerick@keyholesoftware.com>
2017-03-06 11:36:30 -0600

    Initial and Runtime/dynamic config of hystrix

--- khs-bjc-servlet/run.sh
+++ khs-bjc-servlet/run.sh
@@ -4,4 +4,9 @@ docker rm -fv server > /dev/null 2>&1
 
 docker network create sample > /dev/null 2>&1 
 
-docker run --name server --network sample -d -p 8081:8080 -v `pwd`/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml:Z -v `pwd`/target/khs-bjc-servlet-1.0.0-SNAPSHOT.war:/usr/local/tomcat/webapps/app.war:Z tomcat:8-jre8 
+docker run --name server --network sample -d -p 8081:8080 \
+    -v `pwd`/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml:Z \
+    -v `pwd`/target/khs-bjc-servlet-1.0.0-SNAPSHOT.war:/usr/local/tomcat/webapps/app.war:Z \
+    -v `pwd`/config.properties:/config.properties:z \
+    -e "CATALINA_OPTS=-Darchaius.configurationSource.additionalUrls=file:///config.properties" \
+    tomcat:8-jre8 
